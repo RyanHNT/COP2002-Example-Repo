2c400a870daf48fafaec6521bce773f4a214735a
RyanHNT <144949201+RyanHNT@users.noreply.github.com>
2024-01-23 21:52:10 -0500

    Add files via upload

---
+++ example.py
@@ -0,0 +1,10 @@
+Python 3.11.7 (tags/v3.11.7:fa7a6f2, Dec  4 2023, 19:24:49) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Tu Huynh")
+Tu Huynh
+>>> print("COP2002-0T2")
+COP2002-0T2
+>>> print("Spring 2024")
+Spring 2024
+>>> print("This is an interactive session!")
+This is an interactive session!
